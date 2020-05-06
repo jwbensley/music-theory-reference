830b4d4096230e89cc0a0227dee9fa1350155c29
jwbensley <jwbensley@gmail.com>
2020-05-06 14:39:25 +0100

    Adding triads as per #1

--- main.py
+++ main.py
@@ -3,11 +3,47 @@
 # pip3 install jinja2
 
 from jinja2 import Environment, FileSystemLoader
+import os
+import shutil
 import sys
 
-file_loader = FileSystemLoader('.')
+file_loader = FileSystemLoader(".")
 env = Environment(loader=file_loader)
-template = env.get_template('./templates/scales.j2')
+
+
+if os.path.isdir("./render"):
+    try:
+        shutil.rmtree("./render")
+    except Exception as e:
+        print("Couldn't remov existing render output folder {}: {}".format(
+            "./render", e)
+        )
+        ###return False
+
+if not os.path.isdir("./render"):
+    try:
+        os.makedirs("./render", exist_ok=True)
+        print("Created directory: {}".format("./render"))
+    except Exception as e:
+        print("Couldn't create directory {}: {}".format("./render", e))
+        ###return False
+
+if not os.path.isdir("./render/scales"):
+    try:
+        os.makedirs("./render/scales", exist_ok=True)
+        print("Created directory: {}".format("./render/scales"))
+    except Exception as e:
+        print("Couldn't create directory {}: {}".format("./render/scales", e))
+        ###return False
+
+if not os.path.isdir("./render/chords"):
+    try:
+        os.makedirs("./render/chords", exist_ok=True)
+        print("Created directory: {}".format("./render/chords"))
+    except Exception as e:
+        print("Couldn't create directory {}: {}".format("./render/chords", e))
+        ###return False
+
 
 notes = ['a', 'b♭', 'b', 'c', 'd♭', 'd', 'e♭', 'e', 'f', 'g♭', 'g', 'a♭']
 
@@ -32,9 +68,8 @@ for note in notes:
             else:
                 data['lily_scale'].append(s)
 
+        template = env.get_template('./templates/scales/main.j2')
         render_output = template.render(data)
-        #print(output)
-
         output_file = "render/scales/" + data['root'] + ".ly"
 
         try:
@@ -51,3 +86,20 @@ for note in notes:
 
         render_file.close()
 
+        template = env.get_template('./templates/chords/main.j2')
+        render_output = template.render(data)
+        output_file = "render/chords/" + data['root'] + ".ly"
+        try:
+            render_file = open(output_file, 'w')
+        except Exception as e:
+            print("Couldn't open output file {}: {}".format(output_file, e))
+            ###return False
+
+        try:
+            render_file.write(render_output)
+        except Exception as e:
+            print("Couldn't write to output file {}: {}".format(output_file, e))
+            ###return False
+
+        render_file.close()
+        
---
+++ reference/chords/c.ly
@@ -0,0 +1,27 @@
+\version "2.18.2"
+\language "english"
+
+\markup { "Triads and Inversions" }
+\markup { \vspace #1 }
+
+\markup { "C Major Triads" }
+\markup { "root, major third, minor third" }
+\score {
+  \new PianoStaff {
+    \clef "treble"
+    <c' e' g'>1-"root"
+    <e' g' c''>1-"1st"
+    <g' c'' e''>1-"2nd"
+  }
+}
+
+\markup { "C Minor Triads" }
+\markup { "root, minor third, major third" }
+\score {
+  \new PianoStaff {
+    \clef "treble"
+    <c' eflat' g'>1-"root"
+    <eflat' g' c''>1-"1st"
+    <g' c'' eflat''>1-"2nd"
+  }
+}
